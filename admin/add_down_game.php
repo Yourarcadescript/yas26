8341caa3acec9846f7ffa0d90b2af2037965c996
Roy Nilsson <rn222cx@student.lnu.se>
2015-08-03 12:05:32 +0200

    change admin to clean

--- admin/add_down_game.php
+++ admin/add_down_game.php
@@ -44,10 +44,10 @@ if (isset($_POST['upload']) && $_POST['upload']=="1") {
 
 	copy($_FILES['file']['tmp_name'][0], $setting['sitepath'] . "/ourgames/img/" . $_FILES['file']['name'][0]);
 
-	$gamename = yasDB_admin($_POST["gamename"]);
-	$gamedescript = yasDB_admin($_POST["description"]);
-	$thumbpath = yasDB_admin("../ourgames/img/" . $_FILES['file']['name'][0]);
-	$gamefilepath = yasDB_admin('../ourgames/games/'. $num . str_replace(GetFileExtension($_FILES['file']['name'][1]), 'zip', $_FILES['file']['name'][1]));
+	$gamename = yasDB_clean($_POST["gamename"]);
+	$gamedescript = yasDB_clean($_POST["description"]);
+	$thumbpath = yasDB_clean("../ourgames/img/" . $_FILES['file']['name'][0]);
+	$gamefilepath = yasDB_clean('../ourgames/games/'. $num . str_replace(GetFileExtension($_FILES['file']['name'][1]), 'zip', $_FILES['file']['name'][1]));
 	$query = yasDB_insert("INSERT INTO downgames (title, description, thumbnail, file) VALUES ('$gamename', '$gamedescript', '$thumbpath', '$gamefilepath')",false);
 	if (!$query){
 		echo("Database Error!");
